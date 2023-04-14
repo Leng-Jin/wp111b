574c5785c9a7599879de8701fa9b9e74bc09f506
Leng-Jin <s111110554@student.nqu.edu.tw>
2023-04-14 10:32:11 +0800

    add count.js

---
+++ homework/javascript/practice1/1.js
@@ -0,0 +1,12 @@
+function isPrime(num) 
+{
+    let t = parseInt(Math.sqrt(num));
+    for (let i = 2; i < t; i++) 
+    {
+        if (num % i == 0) 
+        {
+            return false;
+        }
+    }
+    return t
+}
---
+++ homework/javascript/practice2/2.js
@@ -0,0 +1,8 @@
+function vdot(a,b) 
+{
+    let r = []
+    for (let i=0; i<a.length; i++)
+    {
+        
+    }
+}
