f5d5b6a05e7e3d7782bbfdfefbef148c6d29d0f1
Leng-Jin <125621673+Leng-Jin@users.noreply.github.com>
2023-06-18 03:54:17 +0800

    個人網站.js

--- homework/final/個人網站.js
+++ homework/final/個人網站.js
@@ -41,48 +41,44 @@ function myFunction() {
   
       '#my_project':
       `
-      <p>第一個作品 - 個人首頁</p>
-      <a href="file:///C:/Users/USER/Desktop/%E9%87%91%E5%A4%A7/%E5%A4%A7%E4%B8%80(%E4%B8%8B)/%E7%B6%B2%E9%A0%81%E8%A8%AD%E8%A8%88/text/wp111b/homework/about/aboutme.html">個人首頁</a>
+      <p>第一個作品 - <a href="file:///C:/Users/USER/Desktop/%E9%87%91%E5%A4%A7/%E5%A4%A7%E4%B8%80(%E4%B8%8B)/%E7%B6%B2%E9%A0%81%E8%A8%AD%E8%A8%88/text/wp111b/homework/about/aboutme.html">個人首頁</a> </p>
+      
       
       <br>
       <br>
-      <p>第二個作品 - 註冊表單</p>
-      <a href="file:///C:/Users/USER/Desktop/%E9%87%91%E5%A4%A7/%E5%A4%A7%E4%B8%80(%E4%B8%8B)/%E7%B6%B2%E9%A0%81%E8%A8%AD%E8%A8%88/text/wp111b/homework/about/registration-form.html">註冊表單</a>
-
-      <br>
-      <br>
-      <p>第三個作品 - 期中作業</p>
-      <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/Midterm">chatgpt 寫書</a>
-
-      <br>
-      <br>
-      <p>第四個作品 - JavaScript 程式練習</p>
-      <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/javascript/practice1">JavaScript 程式練習</a>
-
-      <br>
-      <br>
-      <p>第五個作品 - JavaScript 練習 2</p>
-      <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/javascript/practice2">JavaScript 練習 2</a>
-
-      <br>
-      <br>
-      <p>第六個作品 - JavaScript 練習 3</p>
-      <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/javascript/practice3">JavaScript 練習 3</a>
-
-      <br>
-      <br>
-      <p>第七個作品 - 側欄伸縮功能表</p>
-      <a href="https://leng-jin.github.io/wp111b/html/navbarJs.html">側欄伸縮功能表</a>
-      <a href="https://leng-jin.github.io/wp111b/html/navbarCSS.html">側欄伸縮功能表</a>
-
-      <br>
-      <br>
-      <p>第八個作品 - 按下『首頁，註冊，登入，登出』會切換畫面的效果</p>
-      <a href="https://leng-jin.github.io/wp111b/homework/navbar2/navbar2.html#signup">切換畫面</a>
-
-
+      <p>第二個作品 - <a href="file:///C:/Users/USER/Desktop/%E9%87%91%E5%A4%A7/%E5%A4%A7%E4%B8%80(%E4%B8%8B)/%E7%B6%B2%E9%A0%81%E8%A8%AD%E8%A8%88/text/wp111b/homework/about/registration-form.html">註冊表單</a></p>
       
-     
+
+      <br>
+      <br>
+      <p>第三個作品 - 期中作業 <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/Midterm">chatgpt 寫書</a>
+      </p>
+      
+      <br>
+      <br>
+      <p>第四個作品 - <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/javascript/practice1">JavaScript 程式練習</a></p>
+      
+
+      <br>
+      <br>
+      <p>第五個作品 - <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/javascript/practice2">JavaScript 練習 2</a></p>
+      
+
+      <br>
+      <br>
+      <p>第六個作品 - <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/javascript/practice3">JavaScript 練習 3</a>
+      </p>
+      
+      <br>
+      <br>
+      <p>第七個作品 - <a href="https://leng-jin.github.io/wp111b/html/navbarJs.html">側欄伸縮功能表-1</a>
+      <a href="https://leng-jin.github.io/wp111b/html/navbarCSS.html">側欄伸縮功能表-2</a>
+      </p>
+      
+      <br>
+      <br>
+      <p>第八個作品 - <a href="https://leng-jin.github.io/wp111b/homework/navbar2/navbar2.html#signup">切換畫面</a></p>
+           
       `
   }
   window.onhashchange = function() 
