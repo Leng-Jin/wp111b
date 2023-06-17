9e12462d10691efb78aa8f0bc4a305b3d79a28b6
Leng-Jin <125621673+Leng-Jin@users.noreply.github.com>
2023-06-18 04:42:22 +0800

    個人網站.js

--- homework/final/個人網站.js
+++ homework/final/個人網站.js
@@ -28,6 +28,7 @@ function myFunction() {
   
       <tr>
       <img src="瑾.jpg" width="300" height="400"/>
+      <p>姓名:陳稜瑾</p>
       <p>畢業學校:北門高中</p>
       <p>目前就讀學校:金門大學資訊工程</p>
       <p>出生地 : 台南市 </p>
@@ -41,7 +42,7 @@ function myFunction() {
   
       '#my_project':
       `
-      <p>第一個作品 - <a href="file:///C:/Users/USER/Desktop/%E9%87%91%E5%A4%A7/%E5%A4%A7%E4%B8%80(%E4%B8%8B)/%E7%B6%B2%E9%A0%81%E8%A8%AD%E8%A8%88/text/wp111b/homework/about/aboutme.html">個人首頁</a> </p>
+      <p>第一個作品 - <a href="file:///C:/Users/USER/Desktop/%E9%87%91%E5%A4%A7/%E5%A4%A7%E4%B8%80(%E4%B8%8B)/%E7%B6%B2%E9%A0%81%E8%A8%AD%E8%A8%88/text/wp111b/homework/about/aboutme.html">個人首頁</a></p>
       
       
       <br>
@@ -51,9 +52,9 @@ function myFunction() {
 
       <br>
       <br>
-      <p>第三個作品 - 期中作業 <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/Midterm">chatgpt 寫書</a>
-      </p>
+      <p>第三個作品 - 期中作業 <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/Midterm">chatgpt 寫書</a></p>
       
+
       <br>
       <br>
       <p>第四個作品 - <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/javascript/practice1">JavaScript 程式練習</a></p>
@@ -66,19 +67,23 @@ function myFunction() {
 
       <br>
       <br>
-      <p>第六個作品 - <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/javascript/practice3">JavaScript 練習 3</a>
-      </p>
+      <p>第六個作品 - <a href="https://github.com/Leng-Jin/wp111b/tree/main/homework/javascript/practice3">JavaScript 練習 3</a></p>
       
+
       <br>
       <br>
       <p>第七個作品 - <a href="https://leng-jin.github.io/wp111b/html/navbarJs.html">側欄伸縮功能表-1</a>
-      <a href="https://leng-jin.github.io/wp111b/html/navbarCSS.html">側欄伸縮功能表-2</a>
+      <a href="https://leng-jin.github.io/wp111b/html/navbarCSS.html">側欄伸縮功能表-2</a></p>
+
+
+      <br>
+      <br>
+      <p>第八個作品 -  <a href="https://leng-jin.github.io/wp111b/homework/navbar2/navbar2.html#signup">切換畫面</a>
       </p>
+     
+
       
-      <br>
-      <br>
-      <p>第八個作品 - <a href="https://leng-jin.github.io/wp111b/homework/navbar2/navbar2.html#signup">切換畫面</a></p>
-           
+     
       `
   }
   window.onhashchange = function() 
